fdaffd3740777534c8f10009cd09cbaf504c2d6b
roman-kiselenko <roman.kiselenko.dev@gmail.com>
2025-09-13 15:30:00 +0300

    Release 0.0.9
    Signed-off-by: roman-kiselenko <roman.kiselenko.dev@gmail.com>

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -64,7 +64,7 @@ export function LoginForm({ login, className, ...props }: LoginFormProps) {
                   href="https://github.com/roman-kiselenko/teleskopio"
                   className={'text-xs'}
                 >
-                  <span>teleskopio v0.0.8</span>
+                  <span>teleskopio v0.0.9</span>
                 </a>
               </div>
             </div>
